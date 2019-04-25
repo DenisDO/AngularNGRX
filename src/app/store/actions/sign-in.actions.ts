0396b39ed64543d14d3f33126b46a93875e9a66a
Denis Derkach <podstavka128@gmail.com>
2019-04-25 13:06:34 +0300

    feature/actions, add actions for sign in

---
+++ src/app/store/actions/sign-in.actions.ts
@@ -0,0 +1,24 @@
+import { Action } from '@ngrx/store';
+import { IUserCredentials } from 'src/app/shared/interfaces/user-credentials.interface';
+import { ISignInResponse } from 'src/app/shared/interfaces/sign-in-response.inteface';
+
+export enum ESignInActions {
+    SignIn = 'SignIn',
+    SignInSuccess = 'SignInSuccess',
+    SignInFailed = 'SignInFailed'
+}
+
+export class SignIn implements Action {
+    public readonly type = ESignInActions.SignIn;
+    constructor(public payload: IUserCredentials) {}
+}
+
+export class SignInSuccess implements Action {
+    public readonly type = ESignInActions.SignInSuccess;
+    constructor(public payload: ISignInResponse) {}
+}
+
+export class SignInFailed implements Action {
+    public readonly type = ESignInActions.SignInFailed;
+    constructor(public payload: ISignInResponse) {}
+}
