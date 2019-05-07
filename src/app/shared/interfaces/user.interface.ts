0742475c796150c8cee113d007664aa2feda2b34
Denis Derkach <podstavka128@gmail.com>
2019-05-07 21:39:13 +0300

    fix/interfaces, edit improve interfaces for users data

--- src/app/shared/interfaces/user.interface.ts
+++ src/app/shared/interfaces/user.interface.ts
@@ -1,6 +1,23 @@
 export interface IUser {
-    id: number;
-    first_name: string;
-    last_name: string;
-    avatar: string;
+    name: string;
+    surname: string;
+    gender: string;
+    region: string;
+    age: number;
+    title: string;
+    phone: string;
+    birthday: {
+        dmy: string,
+        mdy: string,
+        raw: number
+    };
+    email: string;
+    password: string;
+    credit_card: {
+        expiration: string,
+        number: string,
+        pin: number,
+        security: number
+    };
+    photo: string;
 }
--- src/app/shared/interfaces/users-list-response.interface.ts
+++
@@ -1,9 +0,0 @@
-import { IUser } from './user.interface';
-
-export interface IUsersListResponse {
-    page: number;
-    per_page: number;
-    total: number;
-    total_pages: number;
-    data: IUser[];
-}
