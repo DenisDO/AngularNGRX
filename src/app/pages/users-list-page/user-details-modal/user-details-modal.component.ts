31fd3693fe784c319ad393f9e5e9f40dddde4fc1
Denis Derkach <podstavka128@gmail.com>
2019-05-10 15:05:05 +0300

    feature/userDetails, init component

---
+++ src/app/pages/users-list-page/user-details-modal/user-details-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user-details-modal',
+    templateUrl: './user-details-modal.component.html',
+    styleUrls: ['./user-details-modal.component.scss']
+})
+export class UserDetailsModalComponent {
+
+}
