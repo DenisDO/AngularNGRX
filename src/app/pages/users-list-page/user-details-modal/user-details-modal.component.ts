7b9f48f81718ca5b9ba901d3cd16a1f036655d4d
Denis Derkach <podstavka128@gmail.com>
2019-05-10 21:04:21 +0300

    fix/userDetails: - improve template - add ability to clear current user

--- src/app/pages/users-list-page/user-details-modal/user-details-modal.component.ts
+++ src/app/pages/users-list-page/user-details-modal/user-details-modal.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { IAppState } from 'src/app/store/state/app.state';
+import { selectCurrentUser } from 'src/app/store/selectors/user-details.selectors';
+import { ClearCurrentUser } from 'src/app/store/actions/user-details.actions';
 
 @Component({
     selector: 'app-user-details-modal',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
     styleUrls: ['./user-details-modal.component.scss']
 })
 export class UserDetailsModalComponent {
+    constructor(
+        private store: Store<IAppState>
+    ) {}
 
+    public currentUser$ = this.store.pipe(select(selectCurrentUser));
+
+    clearCurrentUser() {
+        this.store.dispatch(new ClearCurrentUser());
+    }
 }
