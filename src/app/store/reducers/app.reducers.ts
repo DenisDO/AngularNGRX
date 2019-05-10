a392b556154b005d945c275f6b098af136060653
Denis Derkach <podstavka128@gmail.com>
2019-05-10 21:01:16 +0300

    fix/reducers, add currentUser reducers to appReducers

--- src/app/store/reducers/app.reducers.ts
+++ src/app/store/reducers/app.reducers.ts
@@ -3,10 +3,11 @@ import { IAppState } from '../state/app.state';
 import { routerReducer } from '@ngrx/router-store';
 import { signInReducers } from './sign-in.reducers';
 import { usersListReducers } from './users-list.reducers';
+import { userDetailsReducers } from './user-details.reducers';
 
 export const appReducers: ActionReducerMap<IAppState, any> = {
     router: routerReducer,
-    currentUser: null,
+    currentUser: userDetailsReducers,
     usersList: usersListReducers,
     signInResponse: signInReducers
 };
